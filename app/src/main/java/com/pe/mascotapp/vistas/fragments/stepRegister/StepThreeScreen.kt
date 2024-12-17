5003853b84c2a96f47c1d764c88e2c723d08ad23
Alejandro Ramirez <alejandrofortt@gmail.com>
2024-12-17 14:31:39 -0500

    ref: step 3 user image size fixed

--- app/src/main/java/com/pe/mascotapp/vistas/fragments/stepRegister/StepThreeScreen.kt
+++ app/src/main/java/com/pe/mascotapp/vistas/fragments/stepRegister/StepThreeScreen.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,7 +20,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -32,10 +32,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -217,25 +219,23 @@ fun StepThreeScreen(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-
-                    PrimaryButton(
+                    AsyncImage(
+                        model = selectedImage.value ?: R.drawable.ic_camara,
                         modifier = Modifier
-                            .width(158.dp)
-                            .height(158.dp),
-                        content = {
-                            AsyncImage(
-                                model = selectedImage.value ?: R.drawable.ic_camara,
-                                modifier = Modifier.fillMaxSize(),
-                                contentScale = ContentScale.Crop,
-                                contentDescription = ""
-                            )
-                        },
-                        contentPadding = PaddingValues(),
-                        shape = RoundedCornerShape(100),
-                    ) {
-                        launcher.launch("image/*")
-                    }
-
+                            .width(156.dp)
+                            .height(156.dp)
+                            .clip(CircleShape)
+                            .clickable {
+                                launcher.launch("image/*")
+                            },
+                        contentScale = ContentScale.Crop,
+                        contentDescription = null,
+                        placeholder = if (LocalInspectionMode.current) {
+                            painterResource(R.drawable.background_main)
+                        } else {
+                            null
+                        }
+                    )
                     BasicEditTextField(
                         maxWidth = 300.dp,
                         modifier = Modifier

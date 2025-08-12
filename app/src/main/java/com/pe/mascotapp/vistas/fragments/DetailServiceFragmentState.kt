9209cdf5e874470dba76f0bfe3a291ac529fc873
josevalerovegazo <jvalero@bncr.fi.cr>
2025-08-11 21:11:40 -0500

    feat: lista de negocios, detalle de negocio y lista de sedes

--- app/src/main/java/com/pe/mascotapp/vistas/fragments/DetailServiceFragmentState.kt
+++ app/src/main/java/com/pe/mascotapp/vistas/fragments/DetailServiceFragmentState.kt
@@ -4,22 +4,27 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.pe.mascotapp.modelos.request.NegocioSede
+import com.pe.mascotapp.modelos.request.Sede
 import com.pe.mascotapp.vistas.fragments.tabsService.TabOne
 import com.pe.mascotapp.vistas.fragments.tabsService.TabTwo
 
-class DetailServiceFragmentState (fragmentManager: FragmentManager, context: Context):
-    FragmentStatePagerAdapter(fragmentManager)  {
+class DetailServiceFragmentState (fragmentManager: FragmentManager,
+                                  private var data: NegocioSede,
+                                  private  var sedeSeleccionada: Sede,
+                                  context: Context)
+    : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun getItem(position: Int): Fragment {
         when (position) {
             0 -> {
-                return TabOne()
+                return TabOne.newInstance(data, sedeSeleccionada)
             }
             1 -> {
                 return TabTwo()
             }
             2 -> {
-                return TabOne()
+                return TabOne.newInstance(data, sedeSeleccionada)
             }
 
         }
@@ -31,14 +36,9 @@ class DetailServiceFragmentState (fragmentManager: FragmentManager, context: Con
     }
 
     override fun getItemPosition(`object`: Any): Int {
-
-        /*if (`object` is StepThree){
-            val f = `object`
-            f.updateImage()
-        }*/
-
-        return super.getItemPosition(`object`)
+        return POSITION_NONE
     }
+
 
     override fun getPageTitle(position: Int): CharSequence? {
         //return super.getPageTitle(position)
@@ -57,5 +57,11 @@ class DetailServiceFragmentState (fragmentManager: FragmentManager, context: Con
 
         })
         return title.lowercase()
+    }
+
+    fun updateData(newData: NegocioSede, newSede: Sede) {
+        data = newData
+        sedeSeleccionada = newSede
+        notifyDataSetChanged()
     }
 }

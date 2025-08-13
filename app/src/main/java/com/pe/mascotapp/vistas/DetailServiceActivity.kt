2dc0d26b7ea6dba69b3cd3acb4c15b1d51eb6bff
Anthoni Rivera La Rosa <84037807+anthoniriv@users.noreply.github.com>
2025-08-12 23:10:40 -0500

    3ERAPANTALLA Y ADD REVIEW
    3ERAPANTALLA Y ADD REVIEW

--- app/src/main/java/com/pe/mascotapp/vistas/DetailServiceActivity.kt
+++ app/src/main/java/com/pe/mascotapp/vistas/DetailServiceActivity.kt
@@ -3,39 +3,45 @@ package com.pe.mascotapp.vistas
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.WindowManager
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.pe.mascotapp.R
 import com.pe.mascotapp.vistas.fragments.DetailServiceFragmentState
 
-
 class DetailServiceActivity : AppCompatActivity() {
 
-    var tabLayout:TabLayout ?= null
-    var viewPager:ViewPager ?= null
-    var btnContactar:Button ?= null
+    var tabLayout: TabLayout? = null
+    var viewPager: ViewPager? = null
+    var btnContactar: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_service_activity)
-        tabLayout = findViewById<TabLayout>(R.id.tab_layout)
-        viewPager = findViewById<ViewPager>(R.id.viewPStep)
-        btnContactar = findViewById<Button>(R.id.btnContactar)
 
-        viewPager!!.adapter = DetailServiceFragmentState(supportFragmentManager, this);
+        val btnBack: LinearLayout = findViewById(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            onBackPressed()
+        }
+
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager = findViewById(R.id.viewPStep)
+        btnContactar = findViewById(R.id.btnContactar)
+
+        // Configuración del ViewPager y TabLayout
+        viewPager!!.adapter = DetailServiceFragmentState(supportFragmentManager, this)
         tabLayout!!.setupWithViewPager(viewPager)
 
+        // Configuración del botón "Contactar"
         btnContactar!!.setOnClickListener {
             openWhatsappContact("+51969928064")
         }
-
     }
 
+    // Método para abrir WhatsApp con el número de contacto
     fun openWhatsappContact(number: String) {
         val uri = Uri.parse("smsto:$number")
         val i = Intent(Intent.ACTION_SENDTO, uri)
--- app/src/main/java/com/pe/mascotapp/vistas/fragments/tabsService/TabTwo.kt
+++ app/src/main/java/com/pe/mascotapp/vistas/fragments/tabsService/TabTwo.kt
@@ -1,12 +1,13 @@
 package com.pe.mascotapp.vistas.fragments.tabsService
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,18 +16,17 @@ import com.pe.mascotapp.R
 import com.pe.mascotapp.modelos.Categorias
 import com.pe.mascotapp.vistas.ReviewActivity
 import com.pe.mascotapp.vistas.adapters.ComentariosAdapter
+import com.pe.mascotapp.vistas.ComentarioDetailActivity
+import com.pe.mascotapp.vistas.AddReviewActivity
+import kotlin.jvm.java
 
 class TabTwo : Fragment() {
 
     var rcvComentarios: RecyclerView? = null
     var ComentariosAdapterType: ComentariosAdapter? = null
     var categoriasArray: ArrayList<Categorias> = ArrayList()
-
     var btnVerReviews: TextView? = null  // Declaración del botón
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
+    var btnAgregarReview: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,29 +36,46 @@ class TabTwo : Fragment() {
         val view: View = inflater.inflate(R.layout.fragment_step_two, container, false)
 
         // Inicializamos los elementos
-        rcvComentarios = view.findViewById<RecyclerView>(R.id.rcvComentarios)
-        btnVerReviews = view.findViewById(R.id.btnVerReviews)  // Inicialización del botón
+        rcvComentarios = view.findViewById(R.id.rcvComentarios)
+        btnVerReviews = view.findViewById(R.id.btnVerReviews)
+        btnAgregarReview = view.findViewById(R.id.btnAgregarReview)
 
         obtenerData()  // Cargar datos en el RecyclerView
 
         // Configuración de RecyclerView
-        rcvComentarios?.setLayoutManager(LinearLayoutManager(context))
-        ComentariosAdapterType = ComentariosAdapter(categoriasArray) { categorias -> }
-        rcvComentarios?.setAdapter(ComentariosAdapterType)
-        rcvComentarios?.setItemAnimator(DefaultItemAnimator())
+        rcvComentarios?.layoutManager = LinearLayoutManager(context)
 
-        // Configuración del botón
+        ComentariosAdapterType = ComentariosAdapter(categoriasArray) { categoria ->
+            val intent = Intent(requireContext(), ComentarioDetailActivity::class.java).apply {
+                putExtra("categoria_id", categoria.id)
+                putExtra("categoria_titulo", categoria.titulo)
+                putExtra("categoria_desc", categoria.descripcion)
+                putExtra("categoria_img", categoria.img)
+            }
+            startActivity(intent)
+        }
+
+
+        rcvComentarios?.adapter = ComentariosAdapterType
+        rcvComentarios?.itemAnimator = DefaultItemAnimator()
+
         btnVerReviews!!.setOnClickListener {
-            // Iniciar la actividad para ver las reseñas
             val intent = Intent(activity, ReviewActivity::class.java)
             startActivity(intent)
         }
 
+        btnAgregarReview!!.setOnClickListener {
+            val intent = Intent(activity, AddReviewActivity::class.java)
+            startActivity(intent)
+        }
+
+
         return view
     }
 
+
     fun obtenerData() {
-        // Ejemplo de datos, puedes cambiar esto por tus datos reales
+        // Aquí puedes agregar tus datos reales para las categorías y comentarios
         val categorias1 = Categorias()
         categorias1.id = 0
         categorias1.titulo = "Comida y Snacks"
@@ -72,6 +89,56 @@ class TabTwo : Fragment() {
         categorias2.descripcion = "Paseos"
         categorias2.img = "paseo_perros"
         categoriasArray.add(categorias2)
+
+        val categorias3 = Categorias()
+        categorias3.id = 2
+        categorias3.titulo = "Juguetes, ropa y accesorios"
+        categorias3.descripcion = "juguetes y variados"
+        categorias3.img = "juguetes_perros"
+        categoriasArray.add(categorias3)
+
+
+        val categorias4 = Categorias()
+        categorias4.id = 3
+        categorias4.titulo = "Veterinaria"
+        categorias4.descripcion = "Veterinaria"
+        categorias4.img = "veterinaria_img"
+        categoriasArray.add(categorias4)
+
+        val categorias5 = Categorias()
+        categorias5.id = 4
+        categorias5.titulo = "Juguetes, ropa y accesorios"
+        categorias5.descripcion = "juguetes y variados"
+        categorias5.img = "juguetes_perros"
+        categoriasArray.add(categorias5)
+
+        val categorias6 = Categorias()
+        categorias6.id = 5
+        categorias6.titulo = "Juguetes, ropa y accesorios"
+        categorias6.descripcion = "juguetes y variados"
+        categorias6.img = "juguetes_perros"
+        categoriasArray.add(categorias6)
+
+        val categorias7 = Categorias()
+        categorias7.id = 6
+        categorias7.titulo = "Juguetes, ropa y accesorios"
+        categorias7.descripcion = "juguetes y variados"
+        categorias7.img = "juguetes_perros"
+        categoriasArray.add(categorias7)
+
+        val categorias8 = Categorias()
+        categorias8.id = 7
+        categorias8.titulo = "Juguetes, ropa y accesorios"
+        categorias8.descripcion = "juguetes y variados"
+        categorias8.img = "juguetes_perros"
+        categoriasArray.add(categorias8)
+
+        val categorias9 = Categorias()
+        categorias9.id = 8
+        categorias9.titulo = "Cerrar sesión"
+        categorias9.descripcion = "Cerrar sesión"
+        categorias9.img = "ic_baseline_logout_24"
+        categoriasArray.add(categorias9)
     }
 
     companion object {
@@ -83,51 +150,4 @@ class TabTwo : Fragment() {
 }
 
 
-//        val categorias3 = Categorias()
-//        categorias3.id = 2
-//        categorias3.titulo = "Juguetes, ropa y accesorios"
-//        categorias3.descripcion = "juguetes y variados"
-//        categorias3.img = "juguetes_perros"
-//        categoriasArray.add(categorias3)
-//
-//        val categorias4 = Categorias()
-//        categorias4.id = 3
-//        categorias4.titulo = "Veterinaria"
-//        categorias4.descripcion = "Veterinaria"
-//        categorias4.img = "veterinaria_img"
-//        categoriasArray.add(categorias4)
-//
-//        val categorias5 = Categorias()
-//        categorias5.id = 4
-//        categorias5.titulo = "Juguetes, ropa y accesorios"
-//        categorias5.descripcion = "juguetes y variados"
-//        categorias5.img = "juguetes_perros"
-//        categoriasArray.add(categorias5)
-//
-//        val categorias6 = Categorias()
-//        categorias6.id = 5
-//        categorias6.titulo = "Juguetes, ropa y accesorios"
-//        categorias6.descripcion = "juguetes y variados"
-//        categorias6.img = "juguetes_perros"
-//        categoriasArray.add(categorias6)
-//
-//        val categorias7 = Categorias()
-//        categorias7.id = 6
-//        categorias7.titulo = "Juguetes, ropa y accesorios"
-//        categorias7.descripcion = "juguetes y variados"
-//        categorias7.img = "juguetes_perros"
-//        categoriasArray.add(categorias7)
-//
-//        val categorias8 = Categorias()
-//        categorias8.id =7
-//        categorias8.titulo = "Juguetes, ropa y accesorios"
-//        categorias8.descripcion = "juguetes y variados"
-//        categorias8.img = "juguetes_perros"
-//        categoriasArray.add(categorias8)
-//
-//        val categorias9 = Categorias()
-//        categorias9.id =8
-//        categorias9.titulo = "Cerrar sesión"
-//        categorias9.descripcion = "Cerrar sesión"
-//        categorias9.img = "ic_baseline_logout_24"
-//        categoriasArray.add(categorias9)
+
